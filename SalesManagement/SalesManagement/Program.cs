3631bb4a24d9cc6c06894da6ff66f8a2d383f30c
Quang Hoàng Dận <19522092@ms.uit.edu.vn>
2020-12-28 02:34:29 +0700

    Fix chart ThongKe

--- SalesManagement/SalesManagement/Program.cs
+++ SalesManagement/SalesManagement/Program.cs
@@ -21,7 +21,7 @@ namespace SalesManagement
     }
     static class global
     {
-        public static string conString = @"Server=DESKTOP-IRREIHM\SQLEXPRESS;Database=SALES_MANAGEMENT;User Id=sa;Password=thanh08052001;";
+        public static string conString = @"Server=LAPTOP-8IL3N9B7\SQL;Database=SALES_MANAGEMENT;User Id=sa;Password=quang17102001;";
     }
 }
 
