698557cd81261d643469826673fa6fe085549f08
Phuocdev <19522052@gm.uit.edu.vn>
2020-12-30 22:57:18 +0700

    update

--- SalesManagement/SalesManagement/SignUp.Designer.cs
+++ SalesManagement/SalesManagement/SignUp.Designer.cs
@@ -47,6 +47,9 @@
             this.textBox4_HoTen = new System.Windows.Forms.TextBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
+            this.button_UpdateImage = new System.Windows.Forms.Button();
+            this.pictureBox_AnhNV = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_AnhNV)).BeginInit();
             this.SuspendLayout();
             // 
             // button_huyBo
@@ -56,7 +59,7 @@
             this.button_huyBo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_huyBo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_huyBo.ForeColor = System.Drawing.Color.Black;
-            this.button_huyBo.Location = new System.Drawing.Point(408, 575);
+            this.button_huyBo.Location = new System.Drawing.Point(391, 565);
             this.button_huyBo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_huyBo.Name = "button_huyBo";
             this.button_huyBo.Size = new System.Drawing.Size(117, 43);
@@ -71,7 +74,7 @@
             this.label10.AutoSize = true;
             this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(65, 82);
+            this.label10.Location = new System.Drawing.Point(331, 68);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(124, 24);
             this.label10.TabIndex = 49;
@@ -81,7 +84,7 @@
             // 
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(69, 351);
+            this.label4.Location = new System.Drawing.Point(335, 337);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(116, 44);
             this.label4.TabIndex = 47;
@@ -92,7 +95,7 @@
             this.label9.AutoSize = true;
             this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(67, 299);
+            this.label9.Location = new System.Drawing.Point(333, 285);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(63, 24);
             this.label9.TabIndex = 45;
@@ -103,7 +106,7 @@
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(65, 185);
+            this.label8.Location = new System.Drawing.Point(331, 171);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(94, 24);
             this.label8.TabIndex = 43;
@@ -114,7 +117,7 @@
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(65, 241);
+            this.label7.Location = new System.Drawing.Point(331, 227);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(78, 24);
             this.label7.TabIndex = 41;
@@ -125,7 +128,7 @@
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(68, 137);
+            this.label6.Location = new System.Drawing.Point(334, 123);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(66, 24);
             this.label6.TabIndex = 39;
@@ -136,7 +139,7 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(69, 498);
+            this.label5.Location = new System.Drawing.Point(335, 484);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(112, 24);
             this.label5.TabIndex = 37;
@@ -149,10 +152,10 @@
             this.button_signup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_signup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_signup.ForeColor = System.Drawing.Color.Black;
-            this.button_signup.Location = new System.Drawing.Point(173, 575);
+            this.button_signup.Location = new System.Drawing.Point(216, 565);
             this.button_signup.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_signup.Name = "button_signup";
-            this.button_signup.Size = new System.Drawing.Size(213, 43);
+            this.button_signup.Size = new System.Drawing.Size(139, 43);
             this.button_signup.TabIndex = 8;
             this.button_signup.Text = "Đăng Ký";
             this.button_signup.UseVisualStyleBackColor = false;
@@ -163,7 +166,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(68, 444);
+            this.label3.Location = new System.Drawing.Point(334, 430);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(86, 24);
             this.label3.TabIndex = 33;
@@ -172,7 +175,7 @@
             // textBox3_laiMK
             // 
             this.textBox3_laiMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3_laiMK.Location = new System.Drawing.Point(204, 497);
+            this.textBox3_laiMK.Location = new System.Drawing.Point(470, 483);
             this.textBox3_laiMK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3_laiMK.Name = "textBox3_laiMK";
             this.textBox3_laiMK.Size = new System.Drawing.Size(320, 26);
@@ -183,7 +186,7 @@
             // textBox2_matKhau
             // 
             this.textBox2_matKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2_matKhau.Location = new System.Drawing.Point(204, 443);
+            this.textBox2_matKhau.Location = new System.Drawing.Point(470, 429);
             this.textBox2_matKhau.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2_matKhau.Name = "textBox2_matKhau";
             this.textBox2_matKhau.Size = new System.Drawing.Size(320, 26);
@@ -193,7 +196,7 @@
             // textBox5_MaNV
             // 
             this.textBox5_MaNV.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5_MaNV.Location = new System.Drawing.Point(204, 82);
+            this.textBox5_MaNV.Location = new System.Drawing.Point(470, 68);
             this.textBox5_MaNV.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox5_MaNV.Name = "textBox5_MaNV";
             this.textBox5_MaNV.Size = new System.Drawing.Size(320, 26);
@@ -202,7 +205,7 @@
             // textBox_diaChi
             // 
             this.textBox_diaChi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_diaChi.Location = new System.Drawing.Point(204, 347);
+            this.textBox_diaChi.Location = new System.Drawing.Point(470, 333);
             this.textBox_diaChi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_diaChi.Multiline = true;
             this.textBox_diaChi.Name = "textBox_diaChi";
@@ -212,7 +215,7 @@
             // textBox_DienThoai
             // 
             this.textBox_DienThoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_DienThoai.Location = new System.Drawing.Point(204, 297);
+            this.textBox_DienThoai.Location = new System.Drawing.Point(470, 283);
             this.textBox_DienThoai.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox_DienThoai.Name = "textBox_DienThoai";
             this.textBox_DienThoai.Size = new System.Drawing.Size(320, 26);
@@ -228,7 +231,7 @@
             this.comboBox2.Items.AddRange(new object[] {
             "Nam",
             "Nữ"});
-            this.comboBox2.Location = new System.Drawing.Point(204, 239);
+            this.comboBox2.Location = new System.Drawing.Point(470, 225);
             this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(181, 28);
@@ -237,7 +240,7 @@
             // textBox4_HoTen
             // 
             this.textBox4_HoTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4_HoTen.Location = new System.Drawing.Point(204, 130);
+            this.textBox4_HoTen.Location = new System.Drawing.Point(470, 116);
             this.textBox4_HoTen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox4_HoTen.Name = "textBox4_HoTen";
             this.textBox4_HoTen.Size = new System.Drawing.Size(320, 26);
@@ -247,8 +250,8 @@
             // 
             this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(204, 186);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Location = new System.Drawing.Point(470, 172);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(181, 26);
             this.dateTimePicker1.TabIndex = 2;
@@ -258,11 +261,41 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(261, 11);
+            this.label1.Location = new System.Drawing.Point(335, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(118, 31);
             this.label1.TabIndex = 31;
             this.label1.Text = "Đăng Ký";
+            // 
+            // button_UpdateImage
+            // 
+            this.button_UpdateImage.BackColor = System.Drawing.Color.Transparent;
+            this.button_UpdateImage.FlatAppearance.BorderSize = 0;
+            this.button_UpdateImage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_UpdateImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_UpdateImage.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button_UpdateImage.Location = new System.Drawing.Point(97, 358);
+            this.button_UpdateImage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_UpdateImage.Name = "button_UpdateImage";
+            this.button_UpdateImage.Size = new System.Drawing.Size(122, 30);
+            this.button_UpdateImage.TabIndex = 53;
+            this.button_UpdateImage.TabStop = false;
+            this.button_UpdateImage.Text = "Chọn ảnh";
+            this.button_UpdateImage.UseVisualStyleBackColor = false;
+            this.button_UpdateImage.Click += new System.EventHandler(this.button_UpdateImage_Click);
+            // 
+            // pictureBox_AnhNV
+            // 
+            this.pictureBox_AnhNV.BackgroundImage = global::SalesManagement.Properties.Resources.nhan_vien;
+            this.pictureBox_AnhNV.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox_AnhNV.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox_AnhNV.Location = new System.Drawing.Point(58, 68);
+            this.pictureBox_AnhNV.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox_AnhNV.Name = "pictureBox_AnhNV";
+            this.pictureBox_AnhNV.Size = new System.Drawing.Size(215, 286);
+            this.pictureBox_AnhNV.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_AnhNV.TabIndex = 52;
+            this.pictureBox_AnhNV.TabStop = false;
             // 
             // SignUp
             // 
@@ -270,7 +303,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AliceBlue;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(641, 640);
+            this.ClientSize = new System.Drawing.Size(834, 651);
+            this.Controls.Add(this.button_UpdateImage);
+            this.Controls.Add(this.pictureBox_AnhNV);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button_huyBo);
             this.Controls.Add(this.textBox5_MaNV);
@@ -294,6 +329,7 @@
             this.Name = "SignUp";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Đăng ký";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_AnhNV)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -320,6 +356,8 @@
         private System.Windows.Forms.TextBox textBox4_HoTen;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_UpdateImage;
+        private System.Windows.Forms.PictureBox pictureBox_AnhNV;
     }
 }
 
