55378d43d93acb329f554d96afd1630727e3764a
Quang Hoàng Dận <19522092@ms.uit.edu.vn>
2021-01-01 14:42:50 +0700

    Revert "fix xuat receipt"
    This revert commit 8c605807da7cc52820756019a79249e7b35848e3.

--- SalesManagement/SalesManagement/SignUp.cs
+++ SalesManagement/SalesManagement/SignUp.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Security.Cryptography;
-//using System.Text;
+using System.Text;
 using System.Runtime.CompilerServices;
 
 namespace SalesManagement
