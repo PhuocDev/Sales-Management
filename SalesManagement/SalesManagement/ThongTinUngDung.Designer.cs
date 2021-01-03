421214b03b52191b55712864c0746e551dc7fa4b
Quang Hoàng Dận <19522092@ms.uit.edu.vn>
2021-01-03 22:20:17 +0700

    Fix bug MaKH

--- SalesManagement/SalesManagement/ThongTinUngDung.Designer.cs
+++ SalesManagement/SalesManagement/ThongTinUngDung.Designer.cs
@@ -189,9 +189,9 @@
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(19, 20);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox1.Size = new System.Drawing.Size(771, 281);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
@@ -209,8 +209,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ThongTinUngDung";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Thông Tin Ứng Dụng";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
